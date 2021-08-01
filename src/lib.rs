a707c6d9aa2433de30174ce527d63dc73a474f01
SOFT <shamel.zaher1@gmail.com>
2021-07-31 18:28:07 -0700

    use the is_empty function in line 48

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,55 @@
-use std::fs;
-use std::error::Error;
 use std::env;
-
+use std::error::Error;
+use std::fs;
 
 // a struct that defines the needed arguments
-pub struct Config 
-{
+pub struct Config {
     pub query: String,
     pub file_name: String,
     pub case_sensitive: bool,
 }
-impl Config 
-{
-    //Passes the arguments 
-    pub fn new(mut  arguments: env::Args) -> Result<Config, &'static str> 
-    {
+impl Config {
+    //Passes the arguments
+    pub fn new(mut arguments: env::Args) -> Result<Config, &'static str> {
         arguments.next();
 
-        let query = match arguments.next(){
+        let query = match arguments.next() {
             Some(argument) => argument,
             None => return Err("Please Provide the query."),
         };
-        let file_name =  match arguments.next(){
+        let file_name = match arguments.next() {
             Some(argument) => argument,
             None => return Err("Please Provide the file name."),
         };
 
-        let case_sensitive= env::var("CASE_INSENSITIVE").is_err();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, file_name, case_sensitive })
+        Ok(Config {
+            query,
+            file_name,
+            case_sensitive,
+        })
     }
 }
 
 /*reads the output of the file (config.file_name),
-decide to call the find_case_insensitive or find function 
+decide to call the find_case_insensitive or find function
 ,and prints the line with the query.
 */
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> 
-{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let file_content = fs::read_to_string(&config.file_name)?;
-    
-    let sensitive_or_not = if config.case_sensitive{
+
+    let sensitive_or_not = if config.case_sensitive {
         find(&config.query, &file_content)
-    }else {
+    } else {
         find_case_insensitive(&config.query, &file_content)
     };
 
-    if sensitive_or_not.len() != 0
-    {
-        for line in sensitive_or_not
-        {
+    if !sensitive_or_not.is_empty() {
+        for line in sensitive_or_not {
             println!("The line that contains your query: \n{}", line);
         }
-    }else {
+    } else {
         print!("Couldn't find: {}", config.query);
         print!("in file: {}", config.file_name);
     }
@@ -60,27 +57,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>
     Ok(())
 }
 
-
 //Iterate through every line and check if the query is there. Return the lines with query.
-pub fn find<'a>
-(query: &'a str, file_content: &'a str) -> Vec<&'a str>
-{
+pub fn find<'a>(query: &'a str, file_content: &'a str) -> Vec<&'a str> {
     file_content
-            .lines()
-            .filter(|line| line.contains(query))
-            .collect()
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 //Same as the find function but case insensitive.
-pub fn find_case_insensitive<'a>
-(query: &'a str, file_content: &'a str) -> Vec<&'a str>
-{
+pub fn find_case_insensitive<'a>(query: &'a str, file_content: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
-    for line in file_content.lines()
-    { 
-        if line.to_lowercase().contains(&query)
-        {
+    for line in file_content.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -88,19 +78,16 @@ pub fn find_case_insensitive<'a>
     results
 }
 
-
 #[cfg(test)]
-mod tests 
-{
+mod tests {
     use super::*;
 
     #[test]
-    //Tests the find function 
+    //Tests the find function
     //if it will return the line that includes the exact same query.
-    fn case_sensitive()
-    {
+    fn case_sensitive() {
         let query = "Love Is A Dagger.";
-        let  file_content = "Love Is A Dagger.
+        let file_content = "Love Is A Dagger.
                           It's A Weapon To Be Wielded Far Away Or Up Close. 
                           You Can See Yourself In It. It's Beautiful. Until It Makes You Bleed.";
 
@@ -108,16 +95,17 @@ mod tests
     }
 
     #[test]
-    //Tests the find_case_insensitive function 
+    //Tests the find_case_insensitive function
     //if it will return the line that includes the "same" query.
-    fn case_insensitive()
-    {
+    fn case_insensitive() {
         let query = "Love Is A DaGGer";
-        let  file_content = "Love Is A Dagger.
+        let file_content = "Love Is A Dagger.
                           It's A Weapon To Be Wielded Far Away Or Up Close. 
                           You Can See Yourself In It. It's Beautiful. Until It Makes You Bleed.";
 
-        assert_eq!(vec!["Love Is A Dagger."], 
-                   find_case_insensitive(query, file_content));
+        assert_eq!(
+            vec!["Love Is A Dagger."],
+            find_case_insensitive(query, file_content)
+        );
     }
 }
