3113821c321790cfc5240841c1c2d1679708dd93
SOFT <shamel.zaher1@gmail.com>
2021-07-31 18:26:53 -0700

    Replace the match statement in line 17 to a if let statement.

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,19 @@ use std::process;
 
 use grep_clone::Config;
 
-fn main() 
-{
+fn main() {
     //Passing the arguments to the Config constructor (Config::new) .
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(0);
     });
 
-    
     println!("Trying to find: {:?}", config.query);
     println!("in file: {:?}\n", config.file_name);
 
     //Calling grep_clone::run function and handling the errors.
-    match grep_clone::run(config)
-    {
-        Err(err) => {eprintln!("An Error ocurred: {}", err.to_string()); process::exit(0);},
-        Ok(_) => (),
+    if let Err(err) = grep_clone::run(config) {
+        eprintln!("An Error ocurred: {}", err.to_string());
+        process::exit(0);
     }
-
 }
